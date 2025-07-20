39f085362fa8067c80176ec7a99c378314d38fe9
WalidAlHassan <walidmunsi@gmail.com>
2025-07-20 12:40:41 +0600

    update

--- Portfolio/wsgi.py
+++ Portfolio/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Demo2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Portfolio.settings')
 
 application = get_wsgi_application()
 
